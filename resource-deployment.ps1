077bf13fc56e8bb37ae65574553e0b0e0c928c28
John Kesinger <john.kesinger@insperity.com>
2020-01-09 16:53:50 -0600

    Test

--- resource-deployment.ps1
+++ resource-deployment.ps1
@@ -1,4 +1,4 @@
-param ($server, [SecureString]$sqluser, [SecureString]$sqlpassword, $resourceGroup, $location, [SecureString]$servicebusnamespace, [SecureString]$databasename)
+param ($server, $sqlusr, [SecureString]$sqlpassword, $resourceGroup, $location, [SecureString]$servicebusnamespace, [SecureString]$databasename)
 # $ = "P@ssword"
 # $ = "adminuser"
 # $ = "nsp-esc-dev"
@@ -8,37 +8,35 @@ param ($server, [SecureString]$sqluser, [SecureString]$sqlpassword, $resourceGro
 # $ = "payroll"
 
 $sqlservername = ConvertTo-SecureString $server -AsPlainText -Force
+$sqluser = ConvertTo-SecureString $sqlusr -AsPlainText -Force
 
-Write-Output "Test"
-Write-Output $sqlservername
+Write-Output "Logging In"
+#az login --service-principal $env:servicePrincipalId -tenent $env:tenentId
 
-# Write-Output "Logging In"
-# #az login --service-principal $env:servicePrincipalId -tenent $env:tenentId
+Write-Output "Creating Resource group"
+az group create --name $resourceGroup --location $location
 
-# Write-Output "Creating Resource group"
-# az group create --name $resourceGroup --location $location
+Write-Output "Creating Azure Service Bus, Queues and Topics"
+az servicebus namespace create --name $servicebusnamespace --resource-group $resourceGroup --location $location --sku standard
+az servicebus queue create --name payroll_queue --namespace-name $servicebusnamespace --resource-group $resourceGroup
 
-# Write-Output "Creating Azure Service Bus, Queues and Topics"
-# az servicebus namespace create --name $servicebusnamespace --resource-group $resourceGroup --location $location --sku standard
-# az servicebus queue create --name payroll_queue --namespace-name $servicebusnamespace --resource-group $resourceGroup
+az servicebus topic create --name paycheck --namespace-name $servicebusnamespace --resource-group $resourceGroup
+az servicebus topic subscription create --name benefit_subscriber --namespace-name $servicebusnamespace -g $resourceGroup --topic-name paycheck
 
-# az servicebus topic create --name paycheck --namespace-name $servicebusnamespace --resource-group $resourceGroup
-# az servicebus topic subscription create --name benefit_subscriber --namespace-name $servicebusnamespace -g $resourceGroup --topic-name paycheck
+az servicebus topic create --name hsa --namespace-name $servicebusnamespace --resource-group $resourceGroup
+az servicebus topic subscription create --name payroll_subscriber --namespace-name $servicebusnamespace -g $resourceGroup --topic-name hsa
 
-# az servicebus topic create --name hsa --namespace-name $servicebusnamespace --resource-group $resourceGroup
-# az servicebus topic subscription create --name payroll_subscriber --namespace-name $servicebusnamespace -g $resourceGroup --topic-name hsa
+Write-Output "Creating SQL Server and Database"
+az sql server create -l $location -g $resourceGroup -n $sqlservername -u $sqluser -p $sqlpassword
+az sql db create -g $resourceGroup -s $sqlservername -n $databasename -e 'Free'
 
-# Write-Output "Creating SQL Server and Database"
-# az sql server create -l $location -g $resourceGroup -n $sqlservername -u $sqluser -p $sqlpassword
-# az sql db create -g $resourceGroup -s $sqlservername -n $databasename -e 'Free'
+$sql = az sql db show-connection-string -s $sqlservername -n $databasename -c odbc
+$keys = az servicebus namespace authorization-rule keys list -g microservice --namespace-name $servicebusnamespace -n RootManageSharedAccessKey | ConvertFrom-Json
 
-# $sql = az sql db show-connection-string -s $sqlservername -n $databasename -c odbc
-# $keys = az servicebus namespace authorization-rule keys list -g microservice --namespace-name $servicebusnamespace -n RootManageSharedAccessKey | ConvertFrom-Json
+$env:DB_CONNECTIONSTRING = $sql -replace "<username>", $sqluser -replace "<password>", $sqlpassword
+$env:SB_CONNECTIONSTRING=$keys[0].primaryConnectionString
+$env:SQL_PW=$sqlpassword
 
-# $env:DB_CONNECTIONSTRING = $sql -replace "<username>", $sqluser -replace "<password>", $sqlpassword
-# $env:SB_CONNECTIONSTRING=$keys[0].primaryConnectionString
-# $env:SQL_PW=$sqlpassword
+Write-Output "Set environmental variables" $env:DB_CONNECTIONSTRING
 
-# Write-Output "Set environmental variables" $env:DB_CONNECTIONSTRING
-
-# docker-compose build
+docker-compose build
